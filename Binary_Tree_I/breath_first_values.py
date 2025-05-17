ce17753b306318cf6a842603dbff2d861169465d
Claudio Hurtado <churtado@fen.uchile.cl>
2025-05-17 16:39:14 -0400

    tree problems

---
+++ Binary_Tree_I/breath_first_values.py
@@ -0,0 +1,25 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+from collections import deque
+
+def breadth_first_values(root):
+    if not root:
+        return []
+
+    queue = deque([root])
+    result = []
+
+    while queue:
+        current = queue.popleft()
+        result.append(current.val)
+
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+
+    return result
